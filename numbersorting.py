13fcd09a2521052db0242916d0e0f900868fda1f
Xinyu <xinyu.zhang@ru.nl>
2024-03-12 11:15:15 +0100

    added triangle formula

--- numbersorting.py
+++ numbersorting.py
@@ -26,3 +26,21 @@ def countCounter(arrayofNum):
 
 countCounter (numbers)
 
+#function to calculate the area of triangles
+def triangleArea(a,b):
+    print(0.5 * a * b)
+    return 0.5 * a * b
+triangleArea(3,4)
+
+#difference between return and print:
+
+def doubleNumber(n):
+    print(2*n)
+
+doubleNumber(triangleArea(3,4))
+# the above line would not run because python won't be able to access the value of triangleArea
+# use "return" instead of "print" so that the value of the triangleArea is stored in python
+# and "return" does not need brackets because it's a control and not a function, anything after "return" that is in the same line will be returned
+# but adding the brackets also works because sometimes what you need to return is longer than one line
+
+    
